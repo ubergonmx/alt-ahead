42a3e70722f85588df49c002d667848ae803809f
Marshal Amieron P. Pinpin <marshalpinpin@gmail.com>
2023-07-26 12:26:44 +0800

    ALT-20 Renamed Alt logo `...tutor` -> `...tutors`

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className="fixed inset-x-0 top-0 z-[10] h-fit border-b border-zinc-300 bg-zinc-100 py-2">
       <div className="max-w-7x1 container mx-auto flex h-full items-center justify-between gap-2">
         <Link href="/" className="flex items-center gap-2">
-          <Image src="/alt-tutor.png" alt="dunno" width={100} height={100} />
+          <Image src="/alt-tutors.png" alt="dunno" width={100} height={100} />
         </Link>
         <div style={{ marginLeft: 180 }}>
           <Link href="/" style={{ marginRight: 20 }}>
