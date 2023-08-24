1bb878230d87ae4dd72e71460c4350a979e723eb
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-08-24 14:48:23 +0800

    ALT-18 Fix for build
    Added non-null assertion to `token.email`
    Changed nextauth_url to secret

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -35,7 +35,7 @@ export const authOptions: NextAuthOptions = {
     async jwt({ token, user }) {
       const dbUser = await db.user.findFirst({
         where: {
-          email: token.email,
+          email: token.email!,
         },
       })
 
