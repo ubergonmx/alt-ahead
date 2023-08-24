9b8faa10d29d7007fe818269c3910c5a14747505
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-08-24 13:40:19 +0800

    ALT-18 Change to logical `&&` on loading

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils'
 import { FC, useState } from 'react'
 import { signIn } from 'next-auth/react'
 import { Icons } from './Icons'
-import { toast, useToast } from '@/hooks/use-toast'
+import { useToast } from '@/hooks/use-toast'
 import Link from 'next/link'
 
 type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement>
@@ -58,7 +58,7 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
         isLoading={isLoading}
         className={cn(buttonVariants({ variant: 'secondary' }), 'm-1 border-neutrals-gray')}
       >
-        {isLoading ?? <Icons.google className="mr-2 h-4 w-4" />}
+        {!isLoading && <Icons.google className="mr-2 h-4 w-4" />}
         Sign in with Google
       </Button>
     </div>
