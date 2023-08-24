705516c81b08355f63b1ae145baa550670d02932
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-08-24 14:32:44 +0800

    ALT-33 Update env
    Used new env lib and replaced `process.env`

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 
 export const envSchema = z.object({
+  NODE_ENV: z.string().nonempty(),
   DATABASE_URL: z.string().nonempty(),
   GOOGLE_CLIENT_ID: z.string().nonempty(),
   GOOGLE_CLIENT_SECRET: z.string().nonempty(),
