204605db41bcec8e435c3afc677aecc9c6c00a3d
Webster Navarro <nwebzx@gmail.com>
2023-08-24 15:04:30 +0800

    ALT-22 Section 4 done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         </div>
       </section>
       <section className="h-[30rem] w-full bg-yellow-orange">
-        <div className="mt-10 flex items-center justify-center">
+        <div className="container mt-10 flex items-center justify-center">
           <div className="m-8 h-80 w-2/5">
             <span className="text-lg font-extrabold uppercase tracking-[0.25em] text-blue">
               Why choose ALT?
@@ -43,7 +43,7 @@ export default function Home() {
             <h1 className="w-[30rem] text-5xl font-extrabold leading-[1.3]">
               Transform learning with <span className="text-red-400">ease</span>.
             </h1>
-            <p className="mt-5 w-[32rem] text-lg">
+            <p className="mt-5 w-[30rem] text-lg">
               With ALT, whether you need help in learning english, mathematics, or any other
               academic area, ALT has you covered.
             </p>
@@ -78,7 +78,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="h-[100rem] w-full bg-yellow-orange-light">
+      <section className="h-[44rem] w-full bg-yellow-orange-light">
         <div className="flex">
           <div className="flex w-1/2 flex-col items-end justify-center p-2">
             <div>
@@ -102,59 +102,71 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div className="mt-10 flex min-h-screen justify-center">
-          <div className="flex h-[45rem] w-[110rem] justify-center rounded-xl bg-blue-green-medium">
-            <div className="mt-10 w-4/5 flex-col text-center">
-              <span className="text-lg font-extrabold uppercase tracking-[0.25em] text-dark-blue">
-                JOIN ALT TUTORS NOW
-              </span>
-              <h1 className="text-5xl font-extrabold leading-[1.3]">
-                Join our team of tutors and help spread the
-                <span className="text-red-400"> love of learning</span>!
-              </h1>
-              <p className="mt-5 text-lg font-medium">
-                Ready to inspire and empower the next generation? Join our team today and make a
-                difference!
-              </p>
-              <div className="mt-12 flex gap-8">
-                <div className="flex h-[20rem] w-1/3 flex-col items-center rounded-lg bg-white p-6">
-                  <div className="h-14 w-14 rounded-full bg-yellow-orange pt-4 text-center">✔</div>
-                  <span className="mt-6 text-3xl font-extrabold text-dark-blue">
-                    Inspire and empower.
-                  </span>
-                  <p className="mt-5 w-[16rem] text-lg font-semibold text-dark-blue">
-                    Joining ALT will help you to connect with students, build their confidence, and
-                    help them succeed!
-                  </p>
-                </div>
-                <div className="flex h-[20rem] w-1/3 flex-col items-center rounded-lg bg-white p-6">
-                  <div className="h-14 w-14 rounded-full bg-yellow-orange pt-4 text-center">✔</div>
-                  <span className="mt-6 text-3xl font-extrabold text-dark-blue">
-                    Accessible and fun!
-                  </span>
-                  <p className="mt-5 w-[18rem] text-lg font-semibold text-dark-blue">
-                    With plenty of resources and support, we&apos;re committed to helping you become
-                    the best tutor you can be!
-                  </p>
-                </div>
-                <div className="flex h-[20rem] w-1/3 flex-col items-center rounded-lg bg-white p-6">
-                  <div className="h-14 w-14 rounded-full bg-yellow-orange pt-4 text-center">✔</div>
-                  <span className="mt-6 text-3xl font-extrabold text-dark-blue">
-                    Flexible schedules.
-                  </span>
-                  <p className="mt-5 w-[18rem] text-lg font-semibold text-dark-blue">
-                    With a flexible schedule and rewarding work, it&apos;s the perfect way to gain
-                    experience and grow in your career.
-                  </p>
-                </div>
+      </section>
+      <section className="flex h-[50rem] w-screen items-center justify-center bg-yellow-orange-light">
+        <div className="flex h-[47rem] w-[110rem] justify-center rounded-xl bg-blue-green-medium">
+          <div className="mt-12 w-4/5 flex-col text-center">
+            <span className="text-lg font-extrabold uppercase tracking-[0.25em] text-dark-blue">
+              JOIN ALT TUTORS NOW
+            </span>
+            <h1 className="mx-auto w-[50rem] text-5xl font-extrabold leading-[1.3]">
+              Join our team of tutors and help spread the
+              <span className="text-red-400"> love of learning</span>!
+            </h1>
+            <p className="mt-5 text-lg font-medium">
+              Ready to inspire and empower the next generation? Join our team today and make a
+              difference!
+            </p>
+            <div className="mt-12 flex gap-8">
+              <div className="flex h-[20rem] w-1/3 flex-col items-center rounded-lg bg-white p-6">
+                <div className="h-14 w-14 rounded-full bg-yellow-orange pt-4 text-center">✔</div>
+                <span className="mt-6 text-3xl font-extrabold text-dark-blue">
+                  Inspire and empower.
+                </span>
+                <p className="mt-5 w-[16rem] text-lg font-semibold text-dark-blue">
+                  Joining ALT will help you to connect with students, build their confidence, and
+                  help them succeed!
+                </p>
               </div>
-              <div>
-                <Button className="mt-8 w-52 bg-blue text-lg font-semibold tracking-wider">
-                  Become a Tutor!
-                </Button>
+              <div className="flex h-[20rem] w-1/3 flex-col items-center rounded-lg bg-white p-6">
+                <div className="h-14 w-14 rounded-full bg-yellow-orange pt-4 text-center">✔</div>
+                <span className="mt-6 text-3xl font-extrabold text-dark-blue">
+                  Accessible and fun!
+                </span>
+                <p className="mt-5 w-[18rem] text-lg font-semibold text-dark-blue">
+                  With plenty of resources and support, we&apos;re committed to helping you become
+                  the best tutor you can be!
+                </p>
+              </div>
+              <div className="flex h-[20rem] w-1/3 flex-col items-center rounded-lg bg-white p-6">
+                <div className="h-14 w-14 rounded-full bg-yellow-orange pt-4 text-center">✔</div>
+                <span className="mt-6 text-3xl font-extrabold text-dark-blue">
+                  Flexible schedules.
+                </span>
+                <p className="mt-5 w-[18rem] text-lg font-semibold text-dark-blue">
+                  With a flexible schedule and rewarding work, it&apos;s the perfect way to gain
+                  experience and grow in your career.
+                </p>
               </div>
             </div>
+            <div>
+              <Button className="mt-10 w-52 bg-blue text-lg font-semibold tracking-wider">
+                Become a Tutor!
+              </Button>
+            </div>
           </div>
+        </div>
+      </section>
+      <section className="h-[50rem] w-full justify-center bg-yellow-orange-light">
+        <div className="mt-10 flex flex-col items-center justify-center text-center">
+          <h1 className="text-5xl font-extrabold leading-[1.3]">Join our community!</h1>
+          <p className="mt-5 w-2/5 text-lg font-medium">
+            Calling all curious minds and lifelong learners! Join our vibrant community of learners
+            and embark on an exciting journey of discovery together!
+          </p>
+          <Button className="mt-10 w-52 bg-red-400 text-lg font-semibold tracking-wider">
+            Join Community
+          </Button>
         </div>
       </section>
     </main>
