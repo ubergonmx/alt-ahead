3e8de21923bbab47b949f42da64cb2a85a04def6
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-08-25 20:06:26 +0800

    Temporary fix for Vercel zod error on build time

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -15,8 +15,8 @@ export const authOptions: NextAuthOptions = {
   },
   providers: [
     GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      clientId: env.GOOGLE_CLIENT_ID!,
+      clientSecret: env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
