b514b6ea03435e3abcc2b077755abc8b398a9f34
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-08-25 22:50:57 +0800

    Temporary zod removal for Vercel build

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,18 +1,20 @@
 import { z } from 'zod'
 
-export const envSchema = z.object({
-  NODE_ENV: z.string().nonempty(),
-  DATABASE_URL: z.string().nonempty(),
-  GOOGLE_CLIENT_ID: z.string().nonempty(),
-  GOOGLE_CLIENT_SECRET: z.string().nonempty(),
-  NEXTAUTH_SECRET: z.string().nonempty(),
-})
+// export const envSchema = z.object({
+//   NODE_ENV: z.string().nonempty(),
+//   DATABASE_URL: z.string().nonempty(),
+//   GOOGLE_CLIENT_ID: z.string().nonempty(),
+//   GOOGLE_CLIENT_SECRET: z.string().nonempty(),
+//   NEXTAUTH_SECRET: z.string().nonempty(),
+// })
 
-export const env = (() => {
-  try {
-    return envSchema.parse(process.env)
-  } catch (error) {
-    console.error(error)
-    return process.env
-  }
-})()
+// export const env = (() => {
+//   try {
+//     return envSchema.parse(process.env)
+//   } catch (error) {
+//     console.error(error)
+//     return process.env
+//   }
+// })()
+
+export const env = process.env
